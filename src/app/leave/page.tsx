899f88b5660a5ee7ec75cc14baf55b84370a2a1a
Jacob Jerry Arackal <jacobjerryarackal123@gmail.com>
2025-12-31 00:57:10 +0530

    updated the imports

--- src/app/leave/page.tsx
+++ src/app/leave/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import MainLayout from '@/components/Structure/MainLayout';
-import LeaveManagementContent from '@/components/Leave/LeaveManagementContent';
+import LeaveManagementContent from '@/components/Leave/LeaveManagementContent/LeaveManagementContent';
 
 export default function LeavePage() {
   return (
