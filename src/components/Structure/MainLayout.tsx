067ab2a2425316de90167228b531028cd9c6a78e
Jacob Jerry Arackal <jacobjerryarackal123@gmail.com>
2025-12-30 15:58:11 +0530

    updated Mainlayout

--- src/components/Structure/MainLayout.tsx
+++ src/components/Structure/MainLayout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState } from 'react';
-import { Layout, theme } from 'antd';
+import { Layout } from 'antd';
 import Sidebar from './Sidebar';
 import Header from './Header';
 import Breadcrumb from './Breadcrumb/Breadcrumb';
@@ -15,24 +15,17 @@ interface MainLayoutProps {
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   const [collapsed, setCollapsed] = useState(false);
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
 
   return (
     <Layout className={styles.mainLayout}>
       <Sidebar collapsed={collapsed} />
-      <Layout>
+      <Layout 
+        className={`${styles.siteLayout} ${collapsed ? styles.siteLayoutCollapsed : styles.siteLayoutExpanded}`}
+      >
         <Header collapsed={collapsed} setCollapsed={setCollapsed} />
         <Content className={styles.content}>
           <Breadcrumb />
-          <div
-            className={styles.contentInner}
-            style={{
-              background: colorBgContainer,
-              borderRadius: borderRadiusLG,
-            }}
-          >
+          <div className={styles.contentInner}>
             {children}
           </div>
         </Content>
