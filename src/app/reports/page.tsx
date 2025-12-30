9919a916f9e02d81710b5917ec11b389da01c920
Jacob Jerry Arackal <jacobjerryarackal123@gmail.com>
2025-12-30 13:49:37 +0530

    updated

---
+++ src/app/reports/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MainLayout from '@/components/Structure/MainLayout';
+import ReportsContent from '@/components/Reports/ReportsContent/ReportsContent';
+
+export default function ReportsPage() {
+  return (
+    <MainLayout>
+      <ReportsContent />
+    </MainLayout>
+  );
+}
