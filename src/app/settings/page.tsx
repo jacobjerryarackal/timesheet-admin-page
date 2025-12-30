d0733e1eca9ee2e67bf846768d7640ec1373796c
Jacob Jerry Arackal <jacobjerryarackal123@gmail.com>
2025-12-30 13:59:15 +0530

    updated

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import MainLayout from '@/components/Structure/MainLayout';
+import SettingsContent from '@/components/Settings/SettingsContent';
+
+export default function SettingsPage() {
+  return (
+    <MainLayout>
+      <SettingsContent />
+    </MainLayout>
+  );
+}
